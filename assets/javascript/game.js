7b98d1d3e1024613d48bb8ddc930dfa74cefa5a0
Jessica Stapp <stapp9@gmail.com>
2019-09-30 16:34:38 -0700

    js file is working so that guesses, losses, and wins register

---
+++ assets/javascript/game.js
@@ -0,0 +1,44 @@
+let wins = 0;
+let guesses = 0;
+let losses = 0;
+let computerGuess;
+
+//make an array for the player and computer to guess
+const letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+//reset function - keep track of wins/losses, reset guesses to 0
+function reset() {
+    guesses = 0;
+    //select random letter for the computer
+    let computerGuessIndex = Math.floor(Math.random() * letters.length);
+    computerGuess = letters[computerGuessIndex];
+    console.log("Computer chose: " + computerGuess);
+}
+
+reset();
+
+//function is run when user presses a key
+document.onkeyup = function (event) {
+    //determines which key was pressed
+    var userGuess = event.key;
+    //return userGuess
+    console.log("user chose: " + userGuess)
+
+    //collect results
+    if (userGuess === computerGuess) {
+        wins++
+        reset();
+    } else {
+        guesses++
+
+        if (guesses === 7) {
+            losses++
+            reset()
+        }
+    }
+    console.log("Wins: " + wins);
+    console.log("Guesses: " + guesses);
+    console.log("Losses: " + losses);
+}
+
+// //figure out how to give the user 7 guesses and make it lose after the 7th incorrect guess (for loop and else statement)
