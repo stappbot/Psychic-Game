f7061e729ab0bfa12a3e522682dc1eecb0880bef
Jessica Stapp <stapp9@gmail.com>
2019-09-30 17:16:02 -0700

    display guessed remaining and letters guessed to html page

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,10 +3,19 @@ let guesses = 7;
 let losses = 0;
 let computerGuess;
 
+//$ and camel case is a convention to say you are pointing at something in html
+const $guessesRemain = document.getElementById("guesses-remain")
+const $lettersGuessed = document.getElementById("letters-guessed")
+
 //make an array for the player and computer to guess
 const letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 //make array of user guesses
 let guessed = []
+
+function displayResults() {
+    $guessesRemain.innerText = guesses;
+    $lettersGuessed.innerText = guessed;
+}
 
 //reset function - keep track of wins/losses, reset guesses to 0
 function reset() {
@@ -16,6 +25,7 @@ function reset() {
     let computerGuessIndex = Math.floor(Math.random() * letters.length);
     computerGuess = letters[computerGuessIndex];
     console.log("Game reset, Computer chose: " + computerGuess);
+    displayResults();
 }
 
 reset();
@@ -30,10 +40,12 @@ document.onkeyup = function (event) {
     //collect results
     if (userGuess === computerGuess) {
         wins++
+        //display to html page
         reset();
     } else {
         guesses--
         guessed.push(userGuess);
+        displayResults();
         if (guesses === 0) {
             losses++
             reset()
